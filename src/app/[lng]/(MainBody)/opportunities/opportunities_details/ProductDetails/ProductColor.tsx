29145c5b801281870f1fb3bd32fc7885c4d48114
NomanGhayyur <nomanghayyur96@gmail.com>
2024-02-28 14:07:26 +0500

    aded oppertunity details

---
+++ src/app/[lng]/(MainBody)/opportunities/opportunities_details/ProductDetails/ProductColor.tsx
@@ -0,0 +1,10 @@
+
+export const ProductColor = ({color}:{color:string}) => {
+  
+  return (
+    <>
+      <p className="mt-2"> <span className="fw-bold fs-6">Color :</span> {color}</p>
+      <hr />
+    </>
+  );
+};
---
+++ src/app/[lng]/(MainBody)/opportunities/opportunities_details/ProductDetails/ProductTable.tsx
@@ -0,0 +1,50 @@
+export const ProductTable = ({ data }: { data: any }) => {
+  return (
+    <>
+      <div>
+        <table className="product-page-width">
+          <tbody className="mb-3">
+            <tr>
+              <td>
+                <b>Unit &nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;</b>
+              </td>
+              <td>{data?.package_unit}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Target &nbsp;&nbsp;&nbsp;:</b>
+              </td>
+              <td>{data?.package_target}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Collected &nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;</b>
+              </td>
+              <td>{data?.package_target - data?.package_remaining}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Remaining &nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;</b>
+              </td>
+              <td>{data?.package_remaining}</td>
+            </tr>
+
+            <tr>
+              <td>
+                <b>Box Type &nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;</b>
+              </td>
+              <td>{data?.package_box_type}</td>
+            </tr>
+            <tr>
+              <td>
+                <b>Risk Level &nbsp;&nbsp;&nbsp;: &nbsp;&nbsp;&nbsp;</b>
+              </td>
+              <td>{data?.package_risk_level}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <hr />
+    </>
+  );
+};
